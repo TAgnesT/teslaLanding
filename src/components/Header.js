c1bc075ac4d347cd65b00322d966ae71cabd7d8c
TAgnesT <gmagiic@hotmail.com>
2022-07-04 13:49:57 +0200

    tesla landing page ready

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import './Header.css';
+
+const Header = ({logo, menu}) => {
+  return (
+    <div className='header'>
+        <div className="logo">
+            <a href="/"> <img src={logo} alt="logo" /> </a>
+        </div>
+
+        <div className="nav-links">
+            <a href="/">Model S</a>
+            <a href="/">Model X</a>
+            <a href="/">Solar Roof</a>
+            <a href="/">Solar Panels</a>
+        </div>
+
+        <div className="mobile-menu">
+            <img src={menu} alt="mobile-menu" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
